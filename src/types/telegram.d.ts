9e911a64fbcf0866c335a8d858ee4884bd7c8b2d
Mirfayz <yaneznayu92@gmail.com>
2026-01-12 19:51:07 +0500

    update

--- src/types/telegram.d.ts
+++ src/types/telegram.d.ts
@@ -1,33 +1,38 @@
-declare global {
-  interface Window {
-    Telegram?: {
-      WebApp?: {
-        ready(): void;
-        sendData(data: string): void; 
+export {}
 
-        colorScheme?: 'light' | 'dark';
-        themeParams?: {
-          bg_color?: string;
-          text_color?: string;
-          hint_color?: string;
-          link_color?: string;
-          button_color?: string;
-          button_text_color?: string;
-          secondary_bg_color?: string;
-        };
-        initDataUnsafe?: {
-          user?: {
-            id: number;
-            first_name: string;
-            last_name?: string;
-            username?: string;
-            language_code?: string;
-            is_premium?: boolean;
-          };
-        };
-      };
-    };
+declare global {
+  interface TelegramWebApp {
+    ready(): void
+    expand(): void
+    disableVerticalSwipes(): void
+    sendData(data: string): void
+
+    colorScheme?: 'light' | 'dark'
+    themeParams?: {
+      bg_color?: string
+      text_color?: string
+      hint_color?: string
+      link_color?: string
+      button_color?: string
+      button_text_color?: string
+      secondary_bg_color?: string
+    }
+
+    initDataUnsafe?: {
+      user?: {
+        id: number
+        first_name: string
+        last_name?: string
+        username?: string
+        language_code?: string
+        is_premium?: boolean
+      }
+    }
+  }
+
+  interface Window {
+    Telegram: {
+      WebApp: TelegramWebApp
+    }
   }
 }
-
-export {};
